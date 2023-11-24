26a314a2041d7670c518ccd622e9837df903a43a
sairarojas <131273447+YesidRojas06@users.noreply.github.com>
2023-11-24 14:02:02 -0500

    hola

--- src/components/venta.vue
+++ src/components/venta.vue
@@ -1,327 +1,336 @@
 <template>
     <div class="container-all">
-        <div class="q-pa-md example-row-equal-width">
-            <div class="row">
-                <div class="col">
-                    <div class="bnt-bc">
-                        <q-btn color="green" label="Generar Ticket" @click="mostrarModal"/>
-                    </div>
-                    <div v-if="showmodal" align="center">
-                        <q-select text-color="black" v-model="ruta" :options="optionsRutas" label="Rutas" style="width: 320px; margin-bottom:15px;" />
-                        <q-select v-model="bus" :options="optionsBuses" label="Buses" style="width: 320px; margin-bottom:15px;"/>
-                        <q-input v-model="fecha_departida" label="Fecha para Partida" filled type="date" style="width: 320px; margin-bottom:15px;" />
-                        <q-btn label="Cerrar" color="amber" padding="sm" @click="cerrarModal" />
-                        <q-btn label="Guardar" color="orange-10" padding="sm" @click="generarTicketInfo()" />
-                       </div>
-                    <div v-if="showClienteDiv" class="cliente" align="center">
-                        <q-btn  class="bnt-bc" color="green" label="Buscar Cliente" @click="buscarCliente()" />
-                        <q-input class="label" standout color="green"  v-model="cedula" label="Cedula" placeholder="Cedula del cliente" style="width: 300px" />
-                        <q-input class="label" standout v-model="nombre" label="Nombre" placeholder="Nombre del cliente" style="width: 300px" />
-                        <q-input class="label" standout v-model="telefono" label="Telefono" placeholder="Telefono del cliente"  style="width: 300px"/>
-                        <q-btn  class="btn-c" color="orange-10" label="Confirmar" @click="CrearTicket()" />
-                    </div>
-                </div>
-                <div class="col-6" align="center" style="margin-top: 60px;">
-                    <div v-if="asientos.length" class="row">
-                        <div v-for="i in asientos" :key="i" class="col-3" >
-                            <q-btn push padding="md" size="22px" :value="i" @click="no_asiento = i" :style="{ backgroundColor: no_asiento === i ? 'red' : puestos.includes(i) ? 'red' : 'initial', cursor: puestos.includes(i) ? 'not-allowed' : 'pointer',}" >💺{{ i }}</q-btn>
-                        </div>
-                    </div>
-                </div>
+      <div class="q-pa-md example-row-equal-width">
+        <div class="row">
+          <div class="col">
+            <div class="bnt-bc">
+              <q-btn color="green" label="Generar Ticket" @click="mostrarModal" />
             </div>
+            <div v-if="showmodal" align="center">
+              <q-select
+                text-color="black"
+                v-model="ruta"
+                :options="optionsRutas"
+                label="Rutas"
+                style="width: 320px; margin-bottom:15px;"
+              />
+              <q-select v-model="bus" :options="optionsBuses" label="Buses" style="width: 320px; margin-bottom:15px;" />
+              <q-input v-model="fecha_departida" label="Fecha para Partida" filled type="date" style="width: 320px; margin-bottom:15px;" />
+              <q-btn label="Cerrar" color="amber" padding="sm" @click="cerrarModal" />
+              <q-btn label="Guardar" color="orange-10" padding="sm" @click="generarTicketInfo()" />
+
+            </div>
+            <div v-if="showClienteDiv" class="cliente" align="center">
+              <q-btn class="bnt-bc" color="green" label="Buscar Cliente" @click="buscarCliente" />
+              <q-input class="label" standout color="green" v-model="cedula" label="Cedula" placeholder="Cedula del cliente" style="width: 300px" />
+              <q-input class="label" standout v-model="nombre" label="Nombre" placeholder="Nombre del cliente" style="width: 300px" />
+              <q-input class="label" standout v-model="telefono" label="Telefono" placeholder="Telefono del cliente" style="width: 300px" />
+              <q-btn class="btn-c" color="orange-10" label="Confirmar" @click="CrearTicket()" />
+            </div>
+          </div>
+          <div class="col-6" align="center" style="margin-top: 60px;">
+            <div v-if="asientos.length" class="row">
+              <div v-for="i in asientos" :key="i" class="col-3">
+                <q-btn
+                  padding="md"
+                  size="22px"
+                  :value="i"
+                  @click="no_asiento = i"
+                  :style="{
+                    backgroundColor: no_asiento === i ? 'orange' : puestos.includes(i) ? 'red' : 'initial',
+                    cursor: puestos.includes(i) ? 'not-allowed' : 'pointer',
+                  }"
+                >
+                  💺{{ i }}
+                </q-btn>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</template>
-
-    
-<script setup>
-import { ref, onMounted, watch } from "vue";
-import { useBusStore } from "../stores/buses.js";
-import { useRutaStore } from "../stores/ruta.js";
-import { useClienteStore } from "../stores/clientes.js";
-import { useTicketStore } from "../stores/boleto.js";
-import { useLoginStore } from "../stores/login.js";
-import { useQuasar } from "quasar";
-
-const $q = useQuasar();
-const busStore = useBusStore();
-const rutaStore = useRutaStore();
-const clienteStrore = useClienteStore();
-const ticketStore = useTicketStore();
-const loginStore = useLoginStore();
-
-let fixed = ref(false);
-
-let ruta = ref("");
-let bus = ref("");
-let fecha_departida = ref("");
-let no_asiento = ref(0);
-let showClienteDiv = ref(false);
-let showmodal = ref(false);
-let cedula = ref();
-let nombre = ref("");
-let telefono = ref();
-let buses = ref([]);
-let rutas = ref([]);
-let clientes = ref([]);
-let asientos = ref([]);
-let vendedor = ref([]);
-
-let optionsRutas = ref([]);
-let optionsBuses = ref([]);
-
-// Mostrar Form Rutas Buses y Fecha de partidad
-function mostrarModal() {
-  obtenerRutas();
-  fixed.value = true;
-  showmodal.value = true;
-};
-
-// Cerrar Modal
-function cerrarModal() {
-  fixed.value = false;
-  showmodal.value = false;
-};
-
-// Obtener Buses, Rutas y clientes 
-async function obtenerInfo() {
-  await busStore.obtenerInfoBuses();
-  buses.value = busStore.buses;
-
-  await rutaStore.obtenerInfoRutas();
-  rutas.value = rutaStore.rutas;
-
-  await clienteStrore.obtenerInfoClientes();
-  clientes.value = clienteStrore.clientes;
-};
-
-// Obtener Opciones de Rutas 
-async function obtenerRutas() {
-  try {
-    await rutaStore.obtenerInfoRutas();
-    optionsRutas.value = rutaStore.rutas.map((ruta) => ({
-      label: `${ruta.precio} - ${ruta.origen} - ${ruta.destino}`,
-      value: String(ruta._id),
-    }));
-  } catch (error) {
-    console.log(error);
-  };
-};
-
-// Obtener Opciones de buses
-async function obtenerBuses() {
-  try {
-    await busStore.obtenerInfoBuses();
-    optionsBuses.value = busStore.buses.map((bus) => ({
-      label: `${bus.placa} - ${bus.empresa_asignada} - ${bus.numero_bus}`,
-      value: String(bus._id),
-    }));
-  } catch (error) {
-    console.log(error);
-  };
-};
-
-// Generar listado de Asientos disponibles
-async function generarListaAsientos() {
-  await validarAsientos();
-  if (validacion.value) {
-    const busSeleccionado = buses.value.find((b) => b._id === bus._rawValue.value);
-    if (busSeleccionado) {
-      const numeroAsientos = busSeleccionado.cantidad_asientos;
-      const listaAsientos = [];
-      for (let i = 1; i <= numeroAsientos; i++) {
-        listaAsientos.push(Number(i));
-      };
-      asientos.value = listaAsientos;
-    };
-  };
-};
-
-
-let cliente_id = ref("");
-let validacionCliente = ref(null)
-
-// Buscar Clientes 
-async function buscarCliente() {
-  const clienteEncontrado = clientes.value.find((cliente) => cliente.cedula == cedula.value || cliente.nombre == nombre.value || cliente.telefono == telefono.value);
-  if (clienteEncontrado) {
-    cedula.value = clienteEncontrado.cedula;
-    nombre.value = clienteEncontrado.nombre;
-    telefono.value = clienteEncontrado.telefono;
-    cliente_id.value = clienteEncontrado._id;
-    greatMessage.value = "Cliente Encontrado";
-    showGreat();
-    validacionCliente.value = true;
-  } else {
-    badMessage.value = "No se encontro ningun cliente";
-    showBad();
-    validacionCliente.value = false;
-  }
-}
-
-// Generar lista Asientos 
-async function generarTicketInfo() {
-  fixed.value = false;
-  generarListaAsientos();
-};
-
-// Crear Ticket
-async function CrearTicket() {
-  if (validacionCliente.value == true) {
-    try {
-      showDefault();
-      await ticketStore.postTicket({
-        vendedor_id: String(vendedor.value._id),
-        cliente_id: cliente_id.value,
-        ruta_id: ruta._rawValue.value,
-        bus_id: bus._rawValue.value,
-        no_asiento: no_asiento.value,
-        fecha_departida: fecha_departida.value,
-      });
-      cancelShow();
-      greatMessage.value = "Ticket Agregado";
-      showGreat();
-      fixed.value = false;
-      showmodal.value = false;
-      asientos.value = [];
-      showClienteDiv = false;
-    } catch (error) {
-      cancelShow();
-      badMessage.value = error.response.data.error.errors[0].msg;
-      showBad();
-    };
-  } else {
-    cancelShow();
-    badMessage.value = "Agrega un Cliente"
-    showBad();
-  };
-};
-
-// Obtener Vendedor
-async function obtenerVendedor() {
-  vendedor.value = loginStore.vendedor;
-};
-
-let puestos = ref([]);
-// Buscar Asientos Disponibles
-async function validarAsientos() {
-  validar();
-  if (validacion.value == true) {
-    try {
-      const id_ruta = ruta._rawValue.value;
-      const id_bus = bus._rawValue.value;
-      const fecha = fecha_departida.value;
-      showDefault();
-      await ticketStore.buscarTickets(id_ruta, id_bus, fecha);
-      puestos.value = ticketStore.puestos;
-      cancelShow();
-      greatMessage.value = "Listado Puestos";
-      showGreat();
-    } catch (error) {
-      cancelShow();
-      badMessage.value = error.response.data.error.errors[0].msg;
-      showBad();
-    };
-  };
-};
-
-watch(ruta, () => {
-  obtenerBuses();
-});
-
-watch(no_asiento, () => {
-  showClienteDiv = true;
-});
-
-onMounted(async () => {
-  obtenerInfo();
-  obtenerVendedor();
-});
-
-let greatMessage = ref("");
-let badMessage = ref("");
-
-// Notificacion Buena
-const showGreat = () => {
-  notification = $q.notify({
-    spinner: false,
-    message: greatMessage,
-    timeout: 2000,
-    type: "positive",
-  });
-};
-
-// Notificacion Mala 
-const showBad = () => {
-  notification = $q.notify({
-    spinner: false,
-    message: badMessage,
-    timeout: 2000,
-    type: "negative",
-  });
-};
-
-// Notificacion de Carga
-const showDefault = () => {
-  notification = $q.notify({
-    spinner: true,
-    message: "Please wait...",
-    timeout: 0,
-  });
-};
-
-// Cancelar Notificacion
-const cancelShow = () => {
-  if (notification) {
-    notification();
-  };
-};
-
-let notification = ref(null);
-
-let validacion = ref(false);
-
-async function validar() {
-  if (!ruta.value && !bus.value && !fecha_departida.value) {
-    badMessage.value = "Por favor rellene los campos";
-    showBad();
-  } else if (!ruta.value) {
-    badMessage.value = "Seleccione la ruta";
-    showBad();
-  } else if (!bus.value) {
-    badMessage.value = "Seleccione el bus";
-    showBad();
-  } else if (!fecha_departida.value) {
-    badMessage.value = "Seleccione la fecha de partida";
-    showBad();
-  } else {
-    validacion.value = true;
-  };
-};
-</script>
+  </template>
   
-<style scoped>
-.container-all{
+  <script setup>
+  import { ref, onMounted, watch } from "vue";
+  import { useBusStore } from "../stores/buses.js";
+  import { useRutaStore } from "../stores/ruta.js";
+  import { useClienteStore } from "../stores/clientes.js";
+  import { useTicketStore } from "../stores/boleto.js";
+  import { useLoginStore } from "../stores/login.js";
+  import { useQuasar } from "quasar";
+  
+  const $q = useQuasar();
+  const busStore = useBusStore();
+  const rutaStore = useRutaStore();
+  const clienteStore = useClienteStore();
+  const ticketStore = useTicketStore();
+  const loginStore = useLoginStore();
+  
+  let ruta = ref("");
+  let bus = ref("");
+  let fecha_departida = ref("");
+  let no_asiento = ref(0);
+  let showClienteDiv = ref(false);
+  let showmodal = ref(false);
+  let cedula = ref("");
+  let nombre = ref("");
+  let telefono = ref("");
+  let buses = ref([]);
+  let rutas = ref([]);
+  let clientes = ref([]);
+  let asientos = ref([]);
+  let vendedor = ref([]);
+  
+  let optionsRutas = ref([]);
+  let optionsBuses = ref([]);
+  
+  // Mostrar Form Rutas Buses y Fecha de partida
+  function mostrarModal() {
+    obtenerRutas();
+    showmodal.value = true;
+  }
+  
+  // Cerrar Modal
+  function cerrarModal() {
+    showmodal.value = false;
+  }
+  
+  // Obtener Buses, Rutas y clientes
+  async function obtenerInfo() {
+    await busStore.obtenerInfoBuses();
+    buses.value = busStore.buses;
+  
+    await rutaStore.obtenerInfoRutas();
+    rutas.value = rutaStore.rutas;
+  
+    await clienteStore.getCliente();
+    clientes.value = clienteStore.clientes;
+  }
+  
+  // Obtener Opciones de Rutas
+  async function obtenerRutas() {
+    try {
+      await rutaStore.obtenerInfoRutas();
+      optionsRutas.value = rutaStore.rutas.map((ruta) => ({
+        label: `${ruta.precio} - ${ruta.origen} - ${ruta.destino}`,
+        value: String(ruta._id),
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // Obtener Opciones de buses
+  async function obtenerBuses() {
+    try {
+      await busStore.obtenerInfoBuses();
+      optionsBuses.value = busStore.buses.map((bus) => ({
+        label: `${bus.placa} - ${bus.empresa_asignada} - ${bus.numero_bus}`,
+        value: String(bus._id),
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // Generar listado de Asientos disponibles
+  async function generarListaAsientos() {
+    await validarAsientos();
+    if (validacion.value) {
+      const busSeleccionado = buses.value.find((b) => b._id === bus.value);
+      if (busSeleccionado) {
+        const numeroAsientos = busSeleccionado.cantidad_asientos;
+        const listaAsientos = Array.from({ length: numeroAsientos }, (_, index) => index + 1);
+        asientos.value = listaAsientos;
+      }
+    }
+  }
+  
+  let cliente_id = ref("");
+  let validacionCliente = ref(null);
+  
+  // Buscar Clientes
+  async function buscarCliente() {
+    const clienteEncontrado = clientes.value.find(
+      (cliente) => cliente.cedula == cedula.value || cliente.nombre == nombre.value || cliente.telefono == telefono.value
+    );
+    if (clienteEncontrado) {
+      cedula.value = clienteEncontrado.cedula;
+      nombre.value = clienteEncontrado.nombre;
+      telefono.value = clienteEncontrado.telefono;
+      cliente_id.value = clienteEncontrado._id;
+      greatMessage.value = "Cliente Encontrado";
+      showGreat();
+      validacionCliente.value = true;
+    } else {
+      badMessage.value = "No se encontró ningún cliente";
+      showBad();
+      validacionCliente.value = false;
+    }
+  }
+  
+  // Generar lista Asientos
+  async function generarTicketInfo() {
+    generarListaAsientos();
+  }
+  
+  // Crear Ticket
+  async function CrearTicket() {
+    if (validacionCliente.value == true) {
+      try {
+        showDefault();
+        await ticketStore.postTicket({
+          vendedor_id: String(vendedor.value._id),
+          cliente_id: cliente_id.value,
+          ruta_id: ruta.value,
+          bus_id: bus.value,
+          no_asiento: no_asiento.value,
+          fecha_departida: fecha_departida.value,
+        });
+        cancelShow();
+        greatMessage.value = "Ticket Agregado";
+        showGreat();
+        showmodal.value = false;
+        asientos.value = [];
+        showClienteDiv.value = false;
+      } catch (error) {
+        cancelShow();
+        badMessage.value = error.response.data.error.errors[0].msg;
+        showBad();
+      }
+        } else {
+        cancelShow();
+        badMessage.value = "Selecciona un asiento y agrega un cliente";
+        showBad();
+    }
+  }
+  
+  // Obtener Vendedor
+  async function obtenerVendedor() {
+    vendedor.value = loginStore.vendedor;
+  }
+  
+  let puestos = ref([]);
+  
+  // Buscar Asientos Disponibles
+  async function validarAsientos() {
+    validar();
+    if (validacion.value) {
+      try {
+        const id_ruta = ruta.value;
+        const id_bus = bus.value;
+        const fecha = fecha_departida.value;
+        showDefault();
+        await ticketStore.buscarTickets(id_ruta, id_bus, fecha);
+        puestos.value = boletoStore.puestos;
+        cancelShow();
+        greatMessage.value = "Listado Puestos";
+        showGreat();
+      } catch (error) {
+        console.log(error);
+        cancelShow();
+        badMessage.value = error.response.data.error.errors[0].msg;
+        showBad();
+      }
+    }
+  }
+  
+  watch(ruta, () => {
+    obtenerBuses();
+  });
+  
+  watch(no_asiento, () => {
+    showClienteDiv.value = true;
+  });
+  
+  onMounted(async () => {
+    obtenerInfo();
+    obtenerVendedor();
+  });
+  
+  let greatMessage = ref("");
+  let badMessage = ref("");
+  
+  // Notificacion Buena
+  const showGreat = () => {
+    notification = $q.notify({
+      spinner: false,
+      message: greatMessage.value,
+      timeout: 2000,
+      type: "positive",
+    });
+  };
+  
+  // Notificacion Mala
+  const showBad = () => {
+    notification = $q.notify({
+      spinner: false,
+      message: badMessage.value,
+      timeout: 2000,
+      type: "negative",
+    });
+  };
+  
+  // Notificacion de Carga
+  const showDefault = () => {
+    notification = $q.notify({
+      spinner: true,
+      message: "Please wait...",
+      timeout: 0,
+    });
+  };
+  
+  // Cancelar Notificacion
+  const cancelShow = () => {
+    if (notification) {
+      notification();
+    }
+  };
+  
+  let notification = ref(null);
+  
+  let validacion = ref(false);
+  
+  async function validar() {
+    if (!ruta.value && !bus.value && !fecha_departida.value) {
+      badMessage.value = "Por favor rellene los campos";
+      showBad();
+    } else if (!ruta.value) {
+      badMessage.value = "Seleccione la ruta";
+      showBad();
+    } else if (!bus.value) {
+      badMessage.value = "Seleccione el bus";
+      showBad();
+    } else if (!fecha_departida.value) {
+      badMessage.value = "Seleccione la fecha de partida";
+      showBad();
+    } else {
+      validacion.value = true;
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .container-all {
     margin: 0;
     padding: 0;
     width: 100%;
-}
-  .cliente input{
+  }
+  .cliente input {
     margin-top: 8px;
   }
-  .bnt-bc{
+  .bnt-bc {
     margin-top: 35px;
     margin-bottom: 15px;
   }
-  .btn-c{
+  .btn-c {
     margin-top: 6px;
   }
-  .label{
+  .label {
     background-color: rgba(250, 250, 249, 0.226);
     border-radius: 5px;
-    border: solid gray 1px; 
+    border: solid gray 1px;
     margin: 8px;
   }
-
-
-</style>
+  </style>
--- src/stores/boleto.js
+++ src/stores/boleto.js
@@ -2,99 +2,75 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import { ref } from "vue";
 
-export const useTicketStore = defineStore("ticket", () => {
-  const tickets = ref([]);
-
-  const getTicket = async () => {
-    try {
-      let res = await axios.get(`/ticket/ticket`);
-      tickets.value = res.data.ticket;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const getTicketsPorFechas = async () => {
-    try {
-      let res = await axios.get(`/ticket/tickets`);
-      tickets.value = res.data.ticket;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const getTicketsPorVendedor = async () => {
-    try {
-      let res = await axios.get(`/ticket/tickets/vendedor`);
-      tickets.value = res.data.ticket;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const getTicketsPorCliente = async () => {
-    try {
-      let res = await axios.get(`/ticket/tickets/cliente`);
-      tickets.value = res.data.ticket;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const getRutasPorBus = async () => {
-    try {
-      let res = await axios.get(`/ticket/tickets/rutas`);
-      tickets.value = res.data.ticket;
-    } catch (error) {
-      throw error;
-    }
-  };
-
+export const useTicketStore = defineStore('ticket', () => {
+  const ticket = ref([])
   const postTicket = async (data) => {
     try {
-      let res = await axios.post("ticket/agregar", data);
+      // console.log(token); 
+
+      // const config = {
+      //   headers: {
+      //     Authorization: `Bearer ${token}`
+      //   }
+      // };
+      // console.log(config);
+
+      let res = await axios.post("/ticket/agregar", data);
       return res;
     } catch (error) {
       throw error;
     }
   };
 
-  const putTicket = async (id, data) => {
+  const getTickets = async () => {
     try {
-      let res = await axios.put(`ticket/editarTicket/${id}`, data);
+      let res = await axios.get("/ticket/ticket");
+      ticket.value = res.data.ticket
       return res;
     } catch (error) {
       throw error;
+    };
+  };
+
+  const putEditarTicket = async (id, data) => {
+    try {
+      let res = await axios.put(`/ticket/editarTicket/${id}`, data)
+      return res
+    } catch (error) {
+      throw error
     }
   };
 
-  const putTicketInactivar = async (id) => {
+  const putInactivarTicket = async (id) => {
     try {
-      let res = await axios.put(`ticket/inactivarTicket/${id}`);
-      return res;
+      let res = await axios.put(`/ticket/inactivarTicket/${id}`);
+      return res
     } catch (error) {
-      console.log(error, "Error al cambiar el estado del ticket");
+      throw error
     }
   };
-  const putTicketActivar = async (id) => {
+
+  const putActivarTicket = async (id) => {
     try {
-      let res = await axios.put(`ticket/activarTicket/${id}`);
-      return res;
+      let res = await axios.put(`/ticket/activarTicket/${id}`)
+      return res
     } catch (error) {
-      console.log(error, "Error al cambiar el estado del ticket");
+      throw error
     }
   };
+  const puestos = ref([])
+  const buscarTickets = async (id_ruta, id_bus, fecha) => {
+    try {
+      let res = await axios.get(`/ticket/encontrartickets?id_ruta=${id_ruta}&id_bus=${id_bus}&fecha=${fecha}`)
+      puestos.value = res.data.puestos
+      return res
+    } catch (error) {
+      throw error
+    }
+  }
 
   return {
-    tickets,
-    getTicket,
-    getTicketsPorFechas,
-    getTicketsPorVendedor,
-    getTicketsPorCliente,
-    getRutasPorBus,
-    postTicket,
-    putTicket,
-    putTicketInactivar,
-    putTicketActivar,
+    ticket,puestos,
+    postTicket, getTickets, putEditarTicket, putInactivarTicket, putActivarTicket, buscarTickets
   };
 });
