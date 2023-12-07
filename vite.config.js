67df6cc2419700fe6c33dc9b3efa114068ca9909
lauraRodrigue <>
2023-12-07 14:00:45 -0500

    laura

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+// FILE: vite.config.js
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
@@ -13,12 +16,5 @@ export default defineConfig({
     quasar({
       sassVariables: 'src/quasar-variables.sass'
     })
-  ],
-  css: {
-    preprocessorOptions: {
-      sass: {
-        // Configuración de Sass aquí
-      },
-    },
-  },
-});
+  ]
+})
